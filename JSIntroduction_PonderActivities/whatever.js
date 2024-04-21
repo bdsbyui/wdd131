c803e9845a5acb8aea8c2a5ff57cc13b08979492
7s4e <bryant@7s4e.org>
2024-04-22 06:25:17 +0800

    complete Running Javascript

---
+++ JSIntroduction_PonderActivities/whatever.js
@@ -0,0 +1,8 @@
+// whatever.js
+const myApples = 4;
+const friendApples = 2;
+let total = myApples + friendApples;
+
+document.getElementById("myApples").textContent = myApples;
+document.getElementById("friendApples").textContent = friendApples;
+document.getElementById("total").textContent = total;
