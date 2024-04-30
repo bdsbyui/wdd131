a39e5f5a48d9ff3b862c310b69f8214ef5429919
7s4e <bryant@7s4e.org>
2024-05-01 07:17:27 +0800

    complete Mission Statement II

--- mission/mission.js
+++ mission/mission.js
@@ -1,16 +1,21 @@
 // wdd131/mission/mission.js - Script for Unit 1's Mission Statement
 
-const themeSelector = document.getElementById("theme");
-themeSelector.addEventListener("change", changeTheme);
+const themeSelector = document.getElementById(`theme`);
 
-function changeTheme(event) {
-    const logo = document.getElementById("logo");
-    if (themeSelector.value = "dark") {
-        document.body.classList.add("dark");
-        logo.src = "byui-logo_white.png";
+console.log(`The display theme is '${themeSelector.value}.'`);
+
+themeSelector.addEventListener(`change`, function(event) {
+    const logo = document.getElementById(`logo`);
+
+    if (themeSelector.value === `dark`) {
+        document.body.classList.add(`dark`);
+        logo.src = `byui-logo_white.png`;
     }
+
     else {
-        document.body.classList.remove("dark");
-        logo.src = "byui-logo_blue.webp;"
+        document.body.classList.remove(`dark`);
+        logo.src = `byui-logo_blue.webp`;
     }
-};
+
+    console.log(`The display theme was changed to '${themeSelector.value}.'`);
+});
