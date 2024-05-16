a36f891c4ba9e5b106419c712ca577eeedd9599f
7s4e <bryant@7s4e.org>
2024-05-16 22:39:58 +0800

    complete Cool Pics II

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,16 +1,37 @@
 // coolpics.js - Script for Unit 2's Cool Pics
 
+// Menu script
 const navElement = document.querySelector("nav");
-
-document.querySelector("button").addEventListener(
+document.querySelector("#menu-button").addEventListener(
     "click",
     () => navElement.classList.toggle("hide")
 );
-
 window.addEventListener(
     "resize",
     () => {
         if (window.innerWidth < 600) {navElement.classList.add("hide");}
         else {navElement.classList.remove("hide");}
+    }
+);
+
+// Gallery script
+document.querySelector(".gallery").addEventListener(
+    "click",
+    (event) => {
+        const label = event.target.closest("img").src.split("-", 1);
+        const path = label + "-full.jpeg";
+        const html = `
+            <div class="viewer">
+                <div class="title-bar"><div class="button-container">
+                    <button id="close-button">X</button>
+                </div></div>
+                <img class="modal" alt="${label}" src="${path}">
+            </div>
+        `
+        document.body.insertAdjacentHTML("afterbegin", html);
+        document.querySelector("#close-button").addEventListener(
+            "click",
+            () => document.querySelector(".viewer").remove()
+        );
     }
 );
