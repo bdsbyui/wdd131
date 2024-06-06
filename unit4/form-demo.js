e9af39e57c54b942bfb1578319002789aab795be
7s4e <bryant@7s4e.org>
2024-06-06 20:38:46 +0800

    complete JS Forms

--- unit4/form-demo.js
+++ unit4/form-demo.js
@@ -1,29 +1,25 @@
 // form-demo.js
 
 function validateForm(event) {
-  // get a reference to the form. Because we attached a submit event listener 
-  // to the form itself, we can access the form either through 'event.target', 
-  // or 'this'
   const theForm = event.target;
-  // the default behavior for a form submit is to try and navigate to another 
-  // page where the form would be processed, if a url is not provided it will 
-  // reload the current page. This sometimes is not desirable behavior. One 
-  // case when we might do this is if we think there is bad data in the form.
-  // To keep it from happening we can can call e.preventDefault()
-  // You should always give feedback to the user about what whet wrong so they 
-  // can fix it. We will store the error messages here
   const errors = [];
-  // start by assuming the form is valid.
   let isValid = true;
+
   // add our validations here
-  
-  // if we ran into any problems above valid will be false.
+  if (theForm.payment.value === "credit") {
+    if (theForm.ccNumber.value !== "1234123412341234") {
+      isValid = false;
+      errors.push("Invalid Credit Card Number");
+    }
+  }
+  if (theForm.fullname.value !== "Bob") {
+    isValid = false;
+    errors.push("Your name is not Bob");
+  }
+
   if (!isValid) {
-    //stop the form from being submitted
     event.preventDefault();
-    // show the errors
     showErrors(errors);
-    // return false to let the browser know the form was not submitted.
     return false;
   }
 }
@@ -39,8 +35,6 @@ function togglePaymentDetails() {
   theForm.ccNumber.required = false;
   theForm.ppUsername.required = false;
   
-  // Show the container based on the selected payment method, and add the 
-  // required attribute back.
   if (theForm.payment.value === "credit") {
     ccContainer.classList.remove("hide");
     ccContainer.required = true;
@@ -51,16 +45,16 @@ function togglePaymentDetails() {
   }
 }
 
-// helper function to display our errors.
 function showErrors(errors) {
   const errorEl = document.querySelector(".errors");
   const html = errors.map((error) => `<p>${error}</p>`);
   errorEl.innerHTML = html.join("");
 }
 
-// attach a change event handler to the paymentMethod input
 document
   .querySelector("#payment")
   .addEventListener("change", togglePaymentDetails);
 
-// attach a submit event handler to the form
+document
+  .querySelector("#checkoutForm")
+  .addEventListener("submit", validateForm);
